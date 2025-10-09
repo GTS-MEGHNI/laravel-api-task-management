27df39eaff8af105c4d6de7c486f6398c60502fc
GTS-MEGHNI <meghniucrpg@live.fr>
2025-10-09 20:33:27 +0100

    chore: remove unnecessary frontend files

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', fn (): Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory => view('welcome'));
